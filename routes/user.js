e57fe871f1741bb5c25cc621298bebc46b4c18f1
Isaac  Tingo <newtone480@gmail.com>
2023-03-09 11:01:42 +0300

    Reset password

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { signup, register, login } = require("../controller/user");
+const {
+  signup,
+  register,
+  login,
+  sendForgotPasswordOtp,
+  changePassword,
+} = require("../controller/user");
 const { verifyOTP } = require("../middleware/verify-otp");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/register", verifyOTP, register);
 router.post("/login", login);
+router.post("/forgot-password", sendForgotPasswordOtp);
+router.post("/reset-password", verifyOTP, changePassword);
 
 module.exports = router;
